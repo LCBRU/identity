4179237372b522435e719d5469c982b2897a752f
Richard Bramley <rabramley@gmail.com>
2020-03-31 12:32:34 +0000

    Suffixes no longer added

--- tests/demographics/test_produce_results.py
+++ tests/demographics/test_produce_results.py
@@ -45,23 +45,23 @@ def test__produce_demographics_result(client, faker, mock_email, row_count, exte
     assert os.path.isfile(dr.result_filepath)
 
     for row, expected in zip(dr.iter_result_rows(), dth._person_details):
-        assert row['spine_title'] == expected['title'] + '_SPINE'
-        assert row['spine_forename'] == expected['given_name'] + '_SPINE'
-        assert row['spine_middlenames'] == expected['middle_name'] + '_SPINE'
-        assert row['spine_lastname'] == expected['family_name'] + '_SPINE'
-        assert row['spine_postcode'] == expected['postcode'] + '_SPINE'
-        assert row['spine_address'] == expected['address'] + '_SPINE'
+        assert row['spine_title'] == expected['title']
+        assert row['spine_forename'] == expected['given_name']
+        assert row['spine_middlenames'] == expected['middle_name']
+        assert row['spine_lastname'] == expected['family_name']
+        assert row['spine_postcode'] == expected['postcode']
+        assert row['spine_address'] == expected['address']
         assert parse_date(row['spine_date_of_birth']) == parse_date(expected["date_of_birth"])
         assert parse_date(row['spine_date_of_death']) == parse_date(expected["date_of_death"])
         assert (row['spine_is_deceased'] == 'True') == expected['is_deceased']
-        assert row['spine_current_gp_practice_code'] == expected['current_gp_practice_code'] + '_SPINE'
+        assert row['spine_current_gp_practice_code'] == expected['current_gp_practice_code']
         assert row['spine_message'] or '' == expected['expected_message']
 
-        assert row['pmi_nhs_number'] == expected['nhs_number'] + '_POST_PMI'
-        assert row['pmi_uhl_system_number'] == expected['uhl_system_number'] + '_POST_PMI'
-        assert row['pmi_family_name'] == expected['family_name'] + '_POST_PMI'
-        assert row['pmi_given_name'] == expected['given_name'] + '_POST_PMI'
-        assert row['pmi_gender'] == expected['gender'] + '_POST_PMI'
+        assert row['pmi_nhs_number'] == expected['nhs_number']
+        assert row['pmi_uhl_system_number'] == expected['uhl_system_number']
+        assert row['pmi_family_name'] == expected['family_name']
+        assert row['pmi_given_name'] == expected['given_name']
+        assert row['pmi_gender'] == expected['gender']
         assert parse_date(row['pmi_dob']) == parse_date(expected['date_of_birth'])
         assert parse_date(row['pmi_date_of_death']) == parse_date(expected["date_of_death"])
-        assert row['pmi_postcode'] == expected['postcode'] + '_POST_PMI'
+        assert row['pmi_postcode'] == expected['postcode']
