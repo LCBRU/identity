4e69211d451b301a41fe994e8b360ecd88ae09e7
Richard Bramley <rabramley@gmail.com>
2020-06-04 09:02:54 +0100

    More debugging

--- identity/string_comparisons.py
+++ identity/string_comparisons.py
@@ -22,37 +22,37 @@ def levenshtein_distance(string_a, string_b):
     return result
 
 
-def levenshtein_distance_iter(string_a, string_b):
-    current_app.logger.info(f'levenshtein_distance_iter: a="{string_a}"; b="{string_b}"')
-    # for all i and j, d[i,j] will hold the Levenshtein distance between
-    # the first i characters of s and the first j characters of t
+# def levenshtein_distance_iter(string_a, string_b):
+#     current_app.logger.info(f'levenshtein_distance_iter: a="{string_a}"; b="{string_b}"')
+#     # for all i and j, d[i,j] will hold the Levenshtein distance between
+#     # the first i characters of s and the first j characters of t
 
-    Matrix = [[0 for x in range(w)] for y in range(h)] 
+#     Matrix = [[0 for x in range(w)] for y in range(h)] 
     
-    set each element in d to zero
+#     set each element in d to zero
     
-    // source prefixes can be transformed into empty string by
-    // dropping all characters
-    for i from 1 to m + 1:
-        d[i, 0] := i
+#     // source prefixes can be transformed into empty string by
+#     // dropping all characters
+#     for i from 1 to m + 1:
+#         d[i, 0] := i
     
-    // target prefixes can be reached from empty source prefix
-    // by inserting every character
-    for j from 1 to n + 1:
-        d[0, j] := j
+#     // target prefixes can be reached from empty source prefix
+#     // by inserting every character
+#     for j from 1 to n + 1:
+#         d[0, j] := j
     
-    for j from 1 to n + 1:
-        for i from 1 to m + 1:
-            if s[i] = t[j]:
-                substitutionCost := 0
-            else:
-                substitutionCost := 1
+#     for j from 1 to n + 1:
+#         for i from 1 to m + 1:
+#             if s[i] = t[j]:
+#                 substitutionCost := 0
+#             else:
+#                 substitutionCost := 1
 
-            d[i, j] := minimum(d[i-1, j] + 1,                   // deletion
-                                d[i, j-1] + 1,                   // insertion
-                                d[i-1, j-1] + substitutionCost)  // substitution
+#             d[i, j] := minimum(d[i-1, j] + 1,                   // deletion
+#                                 d[i, j-1] + 1,                   // insertion
+#                                 d[i-1, j-1] + substitutionCost)  // substitution
     
-    return d[m + 1, n + 1]
+#     return d[m + 1, n + 1]
     
 
 def similarity(string_a, string_b):
