c5eedaecfa9a4585fe3c503c7f4133b9c381ff54
Richard Bramley <rabramley@gmail.com>
2020-10-02 16:47:25 +0100

    Testing blinding

--- identity/model/__init__.py
+++ identity/model/__init__.py
@@ -10,6 +10,18 @@ class Study(db.Model):
     def __str__(self):
         return self.name or ""
 
+    def get_blind_ids(self, unblind_id, user):
+        result = []
+
+        for bs in self.blinding_sets:
+            for bt in [t for t in bs.blinding_types if not t.deleted]:
+                blind_id = bt.get_blind_id(unblind_id, user)
+                if blind_id:
+                    result.append(blind_id)
+
+        return result
+
+
 
 class StudyParticipant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- manage.py
+++ manage.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 # Load environment variables from '.env' file.
 load_dotenv()
 
-import os
 from migrate.versioning.shell import main
 from identity.config import BaseConfig
 
