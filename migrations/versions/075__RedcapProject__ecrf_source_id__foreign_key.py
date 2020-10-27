efe109b0dc0e8d15045d7549398daac0e13868d5
Richard Bramley <rabramley@gmail.com>
2020-10-27 17:19:14 +0000

    Add foreign key

---
+++ migrations/versions/075__RedcapProject__ecrf_source_id__foreign_key.py
@@ -0,0 +1,29 @@
+from sqlalchemy import (
+    MetaData,
+    Table,
+    Column,
+    Integer,
+    ForeignKey,
+    DateTime,
+    NVARCHAR,
+)
+from sqlalchemy.schema import ForeignKeyConstraint
+
+meta = MetaData()
+
+
+def upgrade(migrate_engine):
+    meta.bind = migrate_engine
+
+    t = Table("redcap_project", meta, autoload=True)
+    e = Table("ecrf_source", meta, autoload=True)
+
+    t.append_constraint(ForeignKeyConstraint([t.c.ecrf_source_id], [e.c.id], name='fk__redcap_project__ecrf_source_id'))
+
+
+def downgrade(migrate_engine):
+    t = Table("redcap_project", meta, autoload=True)
+    e = Table("ecrf_source", meta, autoload=True)
+
+    con = ForeignKeyConstraint([t.c.ecrf_source_id], [e.c.id], name='fk__redcap_project__ecrf_source_id')
+    con.drop()
