256b31843bc12a0d1f30085a0cabd830a356b380
Richard Bramley <rabramley@gmail.com>
2020-10-28 12:41:13 +0000

    That was supposed to be ansi standard

--- migrations/versions/072__EcrfSource__add_data.py
+++ migrations/versions/072__EcrfSource__add_data.py
@@ -3,7 +3,7 @@ from sqlalchemy.schema import ForeignKeyConstraint
 
 def upgrade(migrate_engine):
     migrate_engine.execute("""
-        INSERT INTO ecrf_source (redcap_project_id, "type", name, last_updated_datetime, last_updated_by_user_id)
+        INSERT INTO ecrf_source (redcap_project_id, type, name, last_updated_datetime, last_updated_by_user_id)
         SELECT
             id,
             'redcap_project',
