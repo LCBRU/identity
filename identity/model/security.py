ea7fbc5238b4971d1556018a707b03254b8bcab2
Richard Bramley <rabramley@gmail.com>
2021-06-09 12:37:53 +0000

    Debug api key

--- identity/model/security.py
+++ identity/model/security.py
@@ -25,7 +25,3 @@ class User(BaseUser):
     studies = db.relationship(
         "Study", secondary=users_studies, collection_class=set, backref=db.backref("users", lazy="joined")
     )
-
-    def __str__(self):
-        return 'user'
-
