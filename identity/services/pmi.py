28acf36367a566f84e9ee5bd22213ff35e124403
Richard Bramley <rabramley@gmail.com>
2023-03-04 11:21:43 +0000

    Include database name

--- identity/services/pmi.py
+++ identity/services/pmi.py
@@ -56,7 +56,7 @@ def get_pmi_from_nhs_number(nhs_number):
         system_numbers = conn.execute(text("""
             SELECT
                 main_pat_id as uhl_system_number
-            FROM [dbo].[UHL_PMI_QUERY_BY_NHS_NUMBER](:id)
+            FROM [PMIS_LIVE].[dbo].[UHL_PMI_QUERY_BY_NHS_NUMBER](:id)
             """), id=nhs_number).fetchall()
 
         pmi_records = {}
@@ -89,7 +89,7 @@ def get_pmi_from_uhl_system_number(uhl_system_number):
                 dob as date_of_birth,
                 date_of_death,
                 postcode
-            FROM [dbo].[UHL_PMI_QUERY_BY_ID](:id)
+            FROM [PMIS_LIVE].[dbo].[UHL_PMI_QUERY_BY_ID](:id)
             """), id=uhl_system_number).fetchall()
 
         if len(pmi_records) > 1:
@@ -116,7 +116,7 @@ def _get_pmi_details_from(id, function):
                 dob as date_of_birth,
                 date_of_death,
                 postcode
-            FROM [dbo].[UHL_PMI_QUERY_BY_ID](:id)
+            FROM [PMIS_LIVE].[dbo].[UHL_PMI_QUERY_BY_ID](:id)
             """), id=id).fetchall()
 
         if len(pmi_records) > 1:
