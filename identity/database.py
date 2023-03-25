4430d46ae501198410d59939c797da7858bdd9af
Richard Bramley <rabramley@gmail.com>
2023-03-25 09:34:23 +0000

    Remove unused redcap db connection

--- identity/database.py
+++ identity/database.py
@@ -16,17 +16,3 @@ def pmi_engine():
     finally:
         engine.dispose()
         current_app.logger.info(f'Disposing PMI engine')
-
-
-@contextmanager
-def redcap_engine(database_name):
-    try:
-        current_app.logger.info(f'Starting REDCap engine for {database_name}')
-        engine = create_engine(
-            f"mysql+pymysql://{current_app.config['REDCAP_USERNAME']}:{current_app.config['REDCAP_PASSWORD']}@{current_app.config['REDCAP_HOST']}/{database_name}",
-            echo=current_app.config['SQLALCHEMY_ECHO'],
-        )
-        yield engine
-    finally:
-        engine.dispose()
-        current_app.logger.info(f'Disposing REDCap engine for {database_name}')
