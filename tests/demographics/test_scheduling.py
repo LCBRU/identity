dcd1220bd9da810fe96ff7d3ce5019cc50b6fef9
Richard Bramley <rabramley@gmail.com>
2026-03-27 17:29:55 +0000

    Simplify testing scheduling

--- tests/demographics/test_scheduling.py
+++ tests/demographics/test_scheduling.py
@@ -1,3 +1,4 @@
+import pytest
 from datetime import datetime, UTC
 from lbrc_flask.database import db
 from identity.demographics import do_lookup_tasks
@@ -6,306 +7,138 @@ from tests.demographics import (
     DemographicsTestHelper,
 )
 
-def test__schedule_lookup_tasks__request_not_found(
-    client,
-    faker,
-    mock_process_demographics_request_data,
-    mock_extract_data,
-    mock_produce_demographics_result,
-    mock_extract_pre_pmi_details,
-    mock_extract_post_pmi_details,
-    mock_log_exception,
-):
 
-    do_lookup_tasks(1)
+class TestScheduling:
 
-    mock_extract_data.delay.assert_not_called()
-    mock_extract_pre_pmi_details.delay.assert_not_called()
-    mock_process_demographics_request_data.delay.assert_not_called()
-    mock_extract_post_pmi_details.delay.assert_not_called()
-    mock_produce_demographics_result.delay.assert_not_called()
-    mock_log_exception.assert_called_once()
+    @pytest.fixture(autouse=True)
+    def fixtures(self, client, faker, mock_process_demographics_request_data, mock_extract_data, mock_produce_demographics_result, mock_extract_pre_pmi_details, mock_extract_post_pmi_details, mock_log_exception):
+        self.client = client
+        self.faker = faker
 
+        self.user = login(self.client, self.faker)
+        self.test_helper = DemographicsTestHelper(faker=self.faker, user=self.user)
 
-def test__schedule_lookup_tasks__in_error(
-    client,
-    faker,
-    mock_process_demographics_request_data,
-    mock_extract_data,
-    mock_produce_demographics_result,
-    mock_extract_pre_pmi_details,
-    mock_extract_post_pmi_details,
-    mock_log_exception,
-):
+        self.all_mocks = {}
+        self.all_mocks['mock_process_demographics_request_data'] = mock_process_demographics_request_data
+        self.all_mocks['mock_extract_data'] = mock_extract_data
+        self.all_mocks['mock_produce_demographics_result'] = mock_produce_demographics_result
+        self.all_mocks['mock_extract_pre_pmi_details'] = mock_extract_pre_pmi_details
+        self.all_mocks['mock_extract_post_pmi_details'] = mock_extract_post_pmi_details
+        self.mock_log_exception = mock_log_exception
 
-    u = login(client, faker)
-    dth = DemographicsTestHelper(faker=faker, user=u)
-    dr = dth.get_demographics_request__uploaded()
-    dr.error_datetime = datetime.now(UTC)
-    db.session.add(dr)
-    db.session.commit()
+    def assert_not_called(self, called=None):
+        called = called or []
 
-    do_lookup_tasks(dr.id)
+        not_called_mocks = [v for k, v in self.all_mocks.items() if k not in called]
+        called_mocks = [v for k, v in self.all_mocks.items() if k in called]
 
-    mock_extract_data.delay.assert_not_called()
-    mock_extract_pre_pmi_details.delay.assert_not_called()
-    mock_process_demographics_request_data.delay.assert_not_called()
-    mock_extract_post_pmi_details.delay.assert_not_called()
-    mock_produce_demographics_result.delay.assert_not_called()
-    mock_log_exception.assert_not_called()
+        print(called_mocks)
 
+        for m in not_called_mocks:
+            m.delay.assert_not_called()
 
-def test__schedule_lookup_tasks__paused(
-    client,
-    faker,
-    mock_process_demographics_request_data,
-    mock_extract_data,
-    mock_produce_demographics_result,
-    mock_extract_pre_pmi_details,
-    mock_extract_post_pmi_details,
-    mock_log_exception,
-):
+        for m in called_mocks:
+            m.delay.assert_called_once()
 
-    u = login(client, faker)
-    dth = DemographicsTestHelper(faker=faker, user=u)
-    dr = dth.get_demographics_request__uploaded()
-    dr.paused_datetime = datetime.now(UTC)
-    db.session.add(dr)
-    db.session.commit()
+        if 'mock_log_exception' in called:
+            self.mock_log_exception.assert_called_once()
+        else:
+            self.mock_log_exception.assert_not_called()
 
-    do_lookup_tasks(dr.id)
+    def test__schedule_lookup_tasks__request_not_found(self):
+        do_lookup_tasks(1)
+        self.assert_not_called(['mock_log_exception'])
 
-    mock_extract_data.delay.assert_not_called()
-    mock_extract_pre_pmi_details.delay.assert_not_called()
-    mock_process_demographics_request_data.delay.assert_not_called()
-    mock_extract_post_pmi_details.delay.assert_not_called()
-    mock_produce_demographics_result.delay.assert_not_called()
-    mock_log_exception.assert_not_called()
+    def test__schedule_lookup_tasks__in_error(self):
+        dr = self.test_helper.get_demographics_request__uploaded()
+        dr.error_datetime = datetime.now(UTC)
+        db.session.add(dr)
+        db.session.commit()
 
+        do_lookup_tasks(dr.id)
 
-def test__schedule_lookup_tasks__deleted(
-    client,
-    faker,
-    mock_process_demographics_request_data,
-    mock_extract_data,
-    mock_produce_demographics_result,
-    mock_extract_pre_pmi_details,
-    mock_extract_post_pmi_details,
-    mock_log_exception,
-):
+        self.assert_not_called()
 
-    u = login(client, faker)
-    dth = DemographicsTestHelper(faker=faker, user=u)
-    dr = dth.get_demographics_request__uploaded()
-    dr.deleted_datetime = datetime.now(UTC)
-    db.session.add(dr)
-    db.session.commit()
+    def test__schedule_lookup_tasks__paused(self):
+        dr = self.test_helper.get_demographics_request__uploaded()
+        dr.paused_datetime = datetime.now(UTC)
+        db.session.add(dr)
+        db.session.commit()
 
-    do_lookup_tasks(dr.id)
+        do_lookup_tasks(dr.id)
 
-    mock_extract_data.delay.assert_not_called()
-    mock_extract_pre_pmi_details.delay.assert_not_called()
-    mock_process_demographics_request_data.delay.assert_not_called()
-    mock_extract_post_pmi_details.delay.assert_not_called()
-    mock_produce_demographics_result.delay.assert_not_called()
-    mock_log_exception.assert_not_called()
+        self.assert_not_called()
 
+    def test__schedule_lookup_tasks__deleted(self):
+        dr = self.test_helper.get_demographics_request__uploaded()
+        dr.deleted_datetime = datetime.now(UTC)
+        db.session.add(dr)
+        db.session.commit()
 
-def test__schedule_lookup_tasks__submitted(
-    client,
-    faker,
-    mock_process_demographics_request_data,
-    mock_extract_data,
-    mock_produce_demographics_result,
-    mock_extract_pre_pmi_details,
-    mock_extract_post_pmi_details,
-    mock_log_exception,
-):
+        do_lookup_tasks(dr.id)
 
-    u = login(client, faker)
-    dth = DemographicsTestHelper(faker=faker, user=u)
-    dr = dth.get_demographics_request__data_extraction()
+        self.assert_not_called()
 
-    do_lookup_tasks(dr.id)
+    def test__schedule_lookup_tasks__submitted(self):
+        dr = self.test_helper.get_demographics_request__data_extraction()
 
-    mock_extract_data.delay.assert_called_once_with(dr.id)
-    mock_extract_pre_pmi_details.delay.assert_not_called()
-    mock_process_demographics_request_data.delay.assert_not_called()
-    mock_extract_post_pmi_details.delay.assert_not_called()
-    mock_produce_demographics_result.delay.assert_not_called()
-    mock_log_exception.assert_not_called()
+        do_lookup_tasks(dr.id)
 
+        self.assert_not_called(['mock_extract_data'])
 
-def test__schedule_lookup_tasks__extracted(
-    client,
-    faker,
-    mock_process_demographics_request_data,
-    mock_extract_data,
-    mock_produce_demographics_result,
-    mock_extract_pre_pmi_details,
-    mock_extract_post_pmi_details,
-    mock_log_exception,
-):
+    def test__schedule_lookup_tasks__extracted(self):
+        dr = self.test_helper.get_demographics_request__pre_pmi_lookup()
 
-    u = login(client, faker)
-    dth = DemographicsTestHelper(faker=faker, user=u)
-    dr = dth.get_demographics_request__pre_pmi_lookup()
+        do_lookup_tasks(dr.id)
 
-    do_lookup_tasks(dr.id)
+        self.assert_not_called(['mock_extract_pre_pmi_details'])
+        self.all_mocks['mock_extract_pre_pmi_details'].delay.assert_called_once_with(dr.id)
 
-    mock_extract_data.delay.assert_not_called()
-    mock_extract_pre_pmi_details.delay.assert_called_once_with(dr.id)
-    mock_process_demographics_request_data.delay.assert_not_called()
-    mock_extract_post_pmi_details.delay.assert_not_called()
-    mock_produce_demographics_result.delay.assert_not_called()
-    mock_log_exception.assert_not_called()
+    def test__schedule_lookup_tasks__extracted__skip_pmi(self):
+        self.test_helper._skip_pmi = True
+        dr = self.test_helper.get_demographics_request__pre_pmi_lookup()
 
+        do_lookup_tasks(dr.id)
 
-def test__schedule_lookup_tasks__extracted__skip_pmi(
-    client,
-    faker,
-    mock_process_demographics_request_data,
-    mock_extract_data,
-    mock_produce_demographics_result,
-    mock_extract_pre_pmi_details,
-    mock_extract_post_pmi_details,
-    mock_log_exception,
-):
+        self.assert_not_called(['mock_process_demographics_request_data'])
+        self.all_mocks['mock_process_demographics_request_data'].delay.assert_called_once_with(dr.id)
 
-    u = login(client, faker)
-    dth = DemographicsTestHelper(faker=faker, user=u, skip_pmi=True)
-    dr = dth.get_demographics_request__pre_pmi_lookup()
+    def test__schedule_lookup_tasks__got_pre_pmi(self):
+        dr = self.test_helper.get_demographics_request__spine_lookup()
 
-    do_lookup_tasks(dr.id)
+        do_lookup_tasks(dr.id)
 
-    mock_extract_data.delay.assert_not_called()
-    mock_extract_pre_pmi_details.delay.assert_not_called()
-    mock_process_demographics_request_data.delay.assert_called_once_with(dr.id)
-    mock_extract_post_pmi_details.delay.assert_not_called()
-    mock_produce_demographics_result.delay.assert_not_called()
-    mock_log_exception.assert_not_called()
+        self.assert_not_called(['mock_process_demographics_request_data'])
+        self.all_mocks['mock_process_demographics_request_data'].delay.assert_called_once_with(dr.id)
 
+    def test__schedule_lookup_tasks__spine_looked_up(self):
+        dr = self.test_helper.get_demographics_request__post_pmi_lookup()
 
-def test__schedule_lookup_tasks__got_pre_pmi(
-    client,
-    faker,
-    mock_process_demographics_request_data,
-    mock_extract_data,
-    mock_produce_demographics_result,
-    mock_extract_pre_pmi_details,
-    mock_extract_post_pmi_details,
-    mock_log_exception,
-):
+        do_lookup_tasks(dr.id)
 
-    u = login(client, faker)
-    dth = DemographicsTestHelper(faker=faker, user=u)
-    dr = dth.get_demographics_request__spine_lookup()
+        self.assert_not_called(['mock_extract_post_pmi_details'])
+        self.all_mocks['mock_extract_post_pmi_details'].delay.assert_called_once_with(dr.id)
 
-    do_lookup_tasks(dr.id)
+    def test__schedule_lookup_tasks__spine_looked_up__skip_pmi(self):
+        self.test_helper._skip_pmi = True
+        dr = self.test_helper.get_demographics_request__post_pmi_lookup()
 
-    mock_extract_data.delay.assert_not_called()
-    mock_extract_pre_pmi_details.delay.assert_not_called()
-    mock_process_demographics_request_data.delay.assert_called_once_with(dr.id)
-    mock_extract_post_pmi_details.delay.assert_not_called()
-    mock_produce_demographics_result.delay.assert_not_called()
-    mock_log_exception.assert_not_called()
+        do_lookup_tasks(dr.id)
 
+        self.assert_not_called(['mock_produce_demographics_result'])
+        self.all_mocks['mock_produce_demographics_result'].delay.assert_called_once_with(dr.id)
 
-def test__schedule_lookup_tasks__spine_looked_up(
-    client,
-    faker,
-    mock_process_demographics_request_data,
-    mock_extract_data,
-    mock_produce_demographics_result,
-    mock_extract_pre_pmi_details,
-    mock_extract_post_pmi_details,
-    mock_log_exception,
-):
+    def test__schedule_lookup_tasks__got_post_pmi(self):
+        dr = self.test_helper.get_demographics_request__create_results()
 
-    u = login(client, faker)
-    dth = DemographicsTestHelper(faker=faker, user=u)
-    dr = dth.get_demographics_request__post_pmi_lookup()
+        do_lookup_tasks(dr.id)
 
-    do_lookup_tasks(dr.id)
+        self.assert_not_called(['mock_produce_demographics_result'])
+        self.all_mocks['mock_produce_demographics_result'].delay.assert_called_once_with(dr.id)
 
-    mock_extract_data.delay.assert_not_called()
-    mock_extract_pre_pmi_details.delay.assert_not_called()
-    mock_process_demographics_request_data.delay.assert_not_called()
-    mock_extract_post_pmi_details.delay.assert_called_once_with(dr.id)
-    mock_produce_demographics_result.delay.assert_not_called()
-    mock_log_exception.assert_not_called()
+    def test__schedule_lookup_tasks__result_created(self):
+        dr = self.test_helper.get_demographics_request__download()
 
+        do_lookup_tasks(dr.id)
 
-def test__schedule_lookup_tasks__spine_looked_up__skip_pmi(
-    client,
-    faker,
-    mock_process_demographics_request_data,
-    mock_extract_data,
-    mock_produce_demographics_result,
-    mock_extract_pre_pmi_details,
-    mock_extract_post_pmi_details,
-    mock_log_exception,
-):
-
-    u = login(client, faker)
-    dth = DemographicsTestHelper(faker=faker, user=u, skip_pmi=True)
-    dr = dth.get_demographics_request__post_pmi_lookup()
-
-    do_lookup_tasks(dr.id)
-
-    mock_extract_data.delay.assert_not_called()
-    mock_extract_pre_pmi_details.delay.assert_not_called()
-    mock_process_demographics_request_data.delay.assert_not_called()
-    mock_extract_post_pmi_details.delay.assert_not_called()
-    mock_produce_demographics_result.delay.assert_called_once_with(dr.id)
-    mock_log_exception.assert_not_called()
-
-
-def test__schedule_lookup_tasks__got_post_pmi(
-    client,
-    faker,
-    mock_process_demographics_request_data,
-    mock_extract_data,
-    mock_produce_demographics_result,
-    mock_extract_pre_pmi_details,
-    mock_extract_post_pmi_details,
-    mock_log_exception,
-):
-
-    u = login(client, faker)
-    dth = DemographicsTestHelper(faker=faker, user=u)
-    dr = dth.get_demographics_request__create_results()
-
-    do_lookup_tasks(dr.id)
-
-    mock_extract_data.delay.assert_not_called()
-    mock_extract_pre_pmi_details.delay.assert_not_called()
-    mock_process_demographics_request_data.delay.assert_not_called()
-    mock_extract_post_pmi_details.delay.assert_not_called()
-    mock_produce_demographics_result.delay.assert_called_once_with(dr.id)
-    mock_log_exception.assert_not_called()
-
-
-def test__schedule_lookup_tasks__result_created(
-    client,
-    faker,
-    mock_process_demographics_request_data,
-    mock_extract_data,
-    mock_produce_demographics_result,
-    mock_extract_pre_pmi_details,
-    mock_extract_post_pmi_details,
-    mock_log_exception,
-):
-
-    u = login(client, faker)
-    dth = DemographicsTestHelper(faker=faker, user=u)
-    dr = dth.get_demographics_request__download()
-
-    do_lookup_tasks(dr.id)
-
-    mock_extract_data.delay.assert_not_called()
-    mock_extract_pre_pmi_details.delay.assert_not_called()
-    mock_process_demographics_request_data.delay.assert_not_called()
-    mock_extract_post_pmi_details.delay.assert_not_called()
-    mock_produce_demographics_result.delay.assert_not_called()
-    mock_log_exception.assert_not_called()
+        self.assert_not_called()
